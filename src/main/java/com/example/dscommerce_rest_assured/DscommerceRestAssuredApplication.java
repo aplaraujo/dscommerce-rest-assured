8ca742a1fa68c7cf62979bce4f6318f2e45fd3a5
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-17 15:43:37 -0300

    first commit

---
+++ src/main/java/com/example/dscommerce_rest_assured/DscommerceRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.example.dscommerce_rest_assured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceRestAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceRestAssuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dscommerce_rest_assured/DscommerceRestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dscommerce_rest_assured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceRestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
